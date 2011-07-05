e299be8ad5f6098294008aa3ca7298525ae9cb92
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-05 19:16:37 +0100

    Remove the default data.

--- public/app/stores/Users.js
+++ public/app/stores/Users.js
@@ -6,11 +6,3 @@ App.stores.users = new Ext.data.Store({
         id: 'sencha-users'
     }
 });
-
-// temporary data, for use until full CRUD is implemented
-App.stores.users.proxy.clear();
-App.stores.users.add({ name: 'Drew Neil', email: 'drew.neil@example.com' });
-App.stores.users.add({ name: 'James Pearce', email: 'james.pearce@example.com' });
-App.stores.users.add({ name: 'Ed Spencer', email: 'ed.spencer@example.com' });
-App.stores.users.add({ name: 'Tommy Mainz', email: 'tommy.mainz@example.com' });
-App.stores.users.sync();
