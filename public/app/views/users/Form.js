ca3747c64215a7b7f13a46a51654a95a610859af
Drew Neil <andrew.jr.neil@gmail.com>
2011-07-13 16:56:47 +0100

    Implement a bare UI: list view and form panel.

---
+++ public/app/views/users/Form.js
@@ -0,0 +1,37 @@
+App.views.UsersForm = Ext.extend(Ext.form.FormPanel, {
+
+    initComponent: function(){
+        var titlebar, cancelButton, saveButton, fields;
+
+        cancelButton = {
+            text: 'cancel',
+            ui: 'back',
+            handler: this.onCancelAction,
+            scope: this
+        };
+
+        titlebar = {
+            id: 'userFormTitlebar',
+            xtype: 'toolbar',
+            title: 'Create user',
+            items: [ cancelButton ]
+        };
+
+        Ext.apply(this, {
+            scroll: 'vertical',
+            dockedItems: [ titlebar ]
+        });
+
+        App.views.UsersForm.superclass.initComponent.call(this);
+    },
+
+    onCancelAction: function() {
+        Ext.dispatch({
+            controller: 'Users',
+            action: 'index'
+        });
+    }
+
+});
+
+Ext.reg('App.views.UsersForm', App.views.UsersForm);
